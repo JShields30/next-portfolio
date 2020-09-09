a7aca686280b9986119c326a86d8b1baa932105b
Joe Shields <shields.john.joseph@gmail.com>
2020-09-09 00:24:07 -0500

    updates

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+const Index = () => {
+  return (
+    <Fragment>
+      <div className="bg-portfolio">
+        Joe Shields
+      </div>
+    </Fragment>
+  )
+}
+
+export default Index;
